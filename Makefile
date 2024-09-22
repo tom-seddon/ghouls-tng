tom-seddon/ghouls-tng

commit bf11f8b2092986b283406c15e82e1ebff5fe6983

Remove wip_builds folder. The releases page has the releases now.

Makefile
@@ -137,15 +137,3 @@ _tom_windows_laptop:
 
 	@$(MAKE) build
 	-curl --connect-timeout 0.25 --silent -G 'http://localhost:48075/reset/b2' --data-urlencode "config=$(CONFIG)"
-
-
-##########################################################################
-##########################################################################
-
-.PHONY:wip_build
-wip_build: OUTPUT_SSD:=$(shell $(SHELLCMD) realpath wip_builds/ghouls-tng-$(shell $(SHELLCMD) strftime -d _ _Y_m_d-_H_M_S)-$(shell $(SHELLCMD) whoami).ssd)
-wip_build:
-	$(_V)$(MAKE) build
-	$(_V)$(SHELLCMD) copy-file $(SSD_OUTPUT) $(OUTPUT_SSD)
-	$(_V)echo $(OUTPUT_SSD)
-

commit 9ad60e00c943e2a8c03663d894b2511c5d6856db

Hopefully fix up CI build, broken by bf11f8b2092986b283406c15e82e1ebff5fe6983.

Makefile
@@ -122,6 +122,16 @@ clean:
 ##########################################################################
 ##########################################################################
 
+.PHONY:build
+ci_build: OUTPUT_SSD=$(error Must specify OUTPUT_SSD)
+ci_build:
+    $(_V)$(MAKE) build
+    $(_V)$(SHELLCMD) copy-file "$(SSD_OUTPUT)" "$(OUTPUT_SSD)"
+    $(_V)echo "$(OUTPUT_SSD)"
+
+##########################################################################
+##########################################################################
+
 .PHONY:_tom_laptop
 # _tom_laptop: CONFIG=Master 128 (MOS 3.20)
 _tom_laptop: CONFIG=B/Acorn 1770

appveyor.yml
@@ -23,7 +23,7 @@ install:
 build_script:
   - ps: $env:OUTPUT_SSD=$env:RELEASE_NAME+".ssd"
   - ps: $env:OUTPUT_ZIP=$env:RELEASE_NAME+".zip"
-  - ps: make wip_build VERBOSE=1 TASS=$HOME/tass64-code/64tass OUTPUT_SSD=$env:OUTPUT_SSD
+  - ps: make ci_build VERBOSE=1 TASS=$HOME/tass64-code/64tass OUTPUT_SSD=$env:OUTPUT_SSD
   - ps: zip -9 $env:OUTPUT_ZIP $env:OUTPUT_SSD
   - env
     